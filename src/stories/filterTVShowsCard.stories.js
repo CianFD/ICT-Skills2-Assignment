dfbec5307e885bc1575c6cc510a845c69b88e3de
Cian Dunne <20091471@mail.wit.ie>
2021-08-21 19:09:43 +0100

    Quick update - added FilterTVShowsCard to Stories.

---
+++ src/stories/filterTVShowsCard.stories.js
@@ -0,0 +1,31 @@
+import React from "react";
+import FilterTVShowsCard from "../components/filterTVShowsCard";
+import { MemoryRouter } from "react-router";
+import { action } from "@storybook/addon-actions";
+import { QueryClientProvider, QueryClient } from "react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 360000,
+      refetchInterval: 360000,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+export default {
+  title: "TV Shows/FilterTVShowsCard",
+  component: FilterTVShowsCard,
+  decorators: [
+    (Story) => <MemoryRouter initialEntries={["/"]}>{Story()}</MemoryRouter>,
+    (Story) => (
+      <QueryClientProvider client={queryClient}>{Story()}</QueryClientProvider>
+    ),
+  ],
+};
+
+export const Basic = () => {
+  return <FilterTVShowsCard onUserInput={action("filter input")} />;
+};
+Basic.storyName = "Default";
